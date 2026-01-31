70acd780cfb73d39c538a6ec45cb221a72a8c464
PalladiumHexafluoride <robert.tadic.08@gmail.com>
2026-01-31 13:14:31 +0100

    First change test

--- src/main/java/net/titaniumhexafluoride/titanium_engines/TitaniumEngines.java
+++ src/main/java/net/titaniumhexafluoride/titanium_engines/TitaniumEngines.java
@@ -20,8 +20,7 @@ public class    TitaniumEngines
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "titanium_engines";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public TitaniumEngines(FMLJavaModLoadingContext context)
     {
