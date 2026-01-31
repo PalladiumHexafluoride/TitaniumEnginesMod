7239884738f4905d955d2248e5f9d9a48534cee2
PalladiumHexafluoride <robert.tadic.08@gmail.com>
2026-01-31 13:11:50 +0100

    intitial commit

--- src/main/java/net/titaniumhexafluoride/titanium_engines/TitaniumEngines.java
+++ src/main/java/net/titaniumhexafluoride/titanium_engines/TitaniumEngines.java
@@ -16,7 +16,7 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TitaniumEngines.MOD_ID)
-public class TitaniumEngines
+public class    TitaniumEngines
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "titanium_engines";
